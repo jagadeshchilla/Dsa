e3975a46f8f21874e2c20b11d088ba516ad0fe5e
jagadeshchilla <chillajagadesh68@gmail.com>
2024-09-03 22:41:36 +0530

    new

--- src/main/java/Arrays/MissingElement.java
+++ src/main/java/Arrays/MissingElement.java
@@ -2,7 +2,7 @@ package Arrays;
 
 public class MissingElement {
     public static int missingNumber(int []a, int N) {
-        int sum = (N * (N + 1)) / 2;
+        /*int sum = (N * (N + 1)) / 2;
 
         //Summation of all array elements:
         int s2 = 0;
@@ -11,9 +11,9 @@ public class MissingElement {
         }
 
         int missingNum = sum - s2;
-        return missingNum;
+        return missingNum;*/
 
-        /*int xor1 = 0, xor2 = 0;
+        int xor1 = 0, xor2 = 0;
 
         for (int i = 0; i < N - 1; i++) {
             xor2 = xor2 ^ a[i]; // XOR of array elements
@@ -21,7 +21,7 @@ public class MissingElement {
         }
         xor1 = xor1 ^ N; //XOR up to [1...N]
 
-        return (xor1 ^ xor2); // the missing number*/
+        return (xor1 ^ xor2); // the missing number
     }
 
     public static void main(String args[]) {
